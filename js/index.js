2bb9f2622be5b632cea680b529f9551b9d5cf1d1
Francis Potter <francis@hathersage.group>
2017-10-15 11:48:55 -0700

    Test commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+/** Draw "windows vs linux" malware chart" **/
+window.onload = function () {
+	var chart = new CanvasJS.Chart("chartContainer", {
+		data: [              
+		{
+			type: "column",
+			dataPoints: [
+				{ label: "Linux",  y: 172207  },
+				{ label: "Android", y: 64405568  },
+				{ label: "Windows", y: 578702687  }
+	
+			]
+		}
+		]
+	});
+	chart.render();
+}
